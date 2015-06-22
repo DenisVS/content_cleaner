8683a549cabae52c324bbda4f955d0d796b387a4
DenisVS <denvsim@gmail.com>
2015-06-22 05:37:55 +0000

    очистка текста перенесена в функцию

--- include/functions.php
+++ include/functions.php
@@ -143,11 +143,11 @@ function removeExcess($text, $startEntry, $endEntry, $required = FALSE) {
     $positionEnd = mb_strpos($contentTail, $endEntry);
   }
   $contentTail = mb_substr($contentTail, $positionEnd + $lenghtEndEntry);
- 
-  echo "AA".$positionStart ." - ". $positionEnd."\n";
+
+  echo "AA" . $positionStart . " - " . $positionEnd . "\n";
 
 
-  if ($required == TRUE && $positionStart !== FALSE && $positionEnd !== FALSE ) {
+  if ($required == TRUE && $positionStart !== FALSE && $positionEnd !== FALSE) {
     $result = $contentHead . $contentTail;
   }
   elseif ($required == FALSE) {
@@ -193,13 +193,13 @@ function minMaxValues($array) {
   foreach ($array as $key => $val) {
     if ($count['max']['value'] == $val) {
       $count['indexes'][] = $key;
-      $count['max']['amount']++; //считаем, сколько ключей с максимальным значением
+      $count['max']['amount'] ++; //считаем, сколько ключей с максимальным значением
     }
   }
   foreach ($array as $key => $val) {
     if ($count['max']['value'] == $val) {
       $count['indexes'][] = $key;
-      $count['min']['amount']++; //считаем, сколько ключей с минимальным значением
+      $count['min']['amount'] ++; //считаем, сколько ключей с минимальным значением
     }
   }
   return $count;
@@ -218,18 +218,153 @@ function lenghtEntryAsterisks($param) {
 function manStyle($param) {
   if (preg_match('/(.*)\S\s--\s\S(.*)\z/m', $param)) {
     return TRUE;
-  }  else {
-    return FALSE;  
+  }
+  else {
+    return FALSE;
   }
 }
- function mb_str_replace($needle, $replacement, $haystack) {
-    $needle_len = mb_strlen($needle);
-    $replacement_len = mb_strlen($replacement);
-    $pos = mb_strpos($haystack, $needle);
-    while ($pos !== false) {
-      $haystack = mb_substr($haystack, 0, $pos) . $replacement
-          . mb_substr($haystack, $pos + $needle_len);
-      $pos = mb_strpos($haystack, $needle, $pos + $replacement_len);
-    }
-    return $haystack;
+
+function mb_str_replace($needle, $replacement, $haystack) {
+  $needle_len = mb_strlen($needle);
+  $replacement_len = mb_strlen($replacement);
+  $pos = mb_strpos($haystack, $needle);
+  while ($pos !== false) {
+    $haystack = mb_substr($haystack, 0, $pos) . $replacement
+        . mb_substr($haystack, $pos + $needle_len);
+    $pos = mb_strpos($haystack, $needle, $pos + $replacement_len);
   }
+  return $haystack;
+}
+
+function cleanPageContent($text) {
+  $text = mb_str_replace("\n\r", "\n", $text);
+  $text = mb_str_replace("\n\n\n\n\n", "\n", $text);
+  $text = mb_str_replace("\n\n\n\n", "\n", $text);
+  $text = mb_str_replace("\n\n\n", "\n", $text);
+  $text = mb_str_replace("\n\n", "\n", $text);
+
+  $text = str_replace('oldchakra.com', 'localhost/out', $text);
+  $text = removeExcess($text, '<meta name=" vi60_defaultclientscript"="">', 'Lord Chaitanya, Balar">', TRUE);
+  $text = removeExcess($text, '<meta name="VI60_', 'Balar">', TRUE);
+  $text = removeExcess($text, '<script', '/script>', TRUE);
+  $text = removeExcess($text, '<script', '/script>', TRUE);
+  $text = removeExcess($text, '<script', '/script>', TRUE);
+  $text = removeExcess($text, '<script', '/script>', TRUE);
+  $text = removeExcess($text, '<script', '/script>', TRUE);
+  $text = removeExcess($text, '<meta name="Description" ', 'content="FrontPage.Editor.Document">', TRUE);
+
+  $text = removeExcess($text, '<!onMouseover --', '/style2003.css">', TRUE);
+  $text = removeExcess($text, 'bgcolor="#FFFFFF"', '"fixed"', TRUE);
+  $text = removeExcess($text, '<p align="center"><a href="http://localhost/out/#top">', '>&nbsp;</p>', TRUE);
+  $text = removeExcess($text, '<p align="Center">All rights', '&nbsp;</p>', TRUE);
+  $text = removeExcess($text, '<p align="Center"><strong><font face="Arial', '</strong>', TRUE);
+  $text = mb_str_replace('<!-- =================TOOLBAR===============  -->', '', $text);
+  $text = mb_str_replace('<link rel="stylesheet" type="text/css" href="http://localhost/out/~styles/style2003.css">', '', $text);
+  $text = mb_str_replace('<link type="text/css" rel="stylesheet" href="/static/css/banner-styles.css"/>', '', $text);
+  $text = mb_str_replace('<meta http-equiv="Content-Type"
+content="text/html; charset=UTF-8>', '<meta http-equiv="Content-Type" content="text/html"; charset=UTF-8>', $text);
+  $text = mb_str_replace('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8>', '<meta http-equiv="Content-Type" content="text/html"; charset=UTF-8>', $text);
+  $text = mb_str_replace('<body text="#000000" link="#0000ff"
+vlink="#0000ff" bgcolor="#ffffff">', '<body>', $text);
+  $text = removeExcess($text, '<!--webbot', '-->', TRUE);
+  $text = removeExcess($text, '<!--webbot', '-->', TRUE);
+  $text = removeExcess($text, '<!--webbot', '-->', TRUE);
+  $text = removeExcess($text, '<!--webbot', '-->', TRUE);
+  $text = removeExcess($text, '<!--webbot', '-->', TRUE);
+  $text = removeExcess($text, '<!--webbot', '-->', TRUE);
+  $text = removeExcess($text, '<!--webbot', '-->', TRUE);
+  $text = removeExcess($text, '<meta name="Description', '">', TRUE);
+  $text = removeExcess($text, '<meta name="KEYWORDS"', '">', TRUE);
+  $text = removeExcess($text, '<table border="1" width="100%">', '</table>', TRUE);
+  //$inFileContent = removeExcess($inFileContent, '', '', TRUE);
+  $text = removeExcess($text, '<table border="0" cellpadding="6" cellspacing="0" width="100%">', '</table>', TRUE);
+
+
+  $text = removeExcess($text, '<p align="center"><strong><font color="#800000"><small>', '</p>', TRUE);
+  $text = removeExcess($text, '<p align="center"><small>For', '</p>', TRUE);
+  $text = removeExcess($text, '<p><font color="#cc0000" face="Times New Roman', '/p>', TRUE);
+  $text = removeExcess($text, '<p align="center"><a href="http://localhost/out/index.html"><img', '></p>', TRUE);
+  $text = removeExcess($text, '<p align="center"><strong><font
+color="#800000"><small>©', '</p>', TRUE);
+  $text = removeExcess($text, '<!-- Pi', 'e -->', TRUE);
+  $text = removeExcess($text, '<p><font color="#cc0000" face="Times', '</p>', TRUE);
+  $text = removeExcess($text, '<p align="left"><i>©', 'p>', TRUE);
+  $text = removeExcess($text, '<meta name="GENERATOR"', 'Document">', TRUE);
+  $text = removeExcess($text, '<p align="center"><small>Hare Krishna!', '</table>', TRUE);
+//$inFileContent = removeExcess($inFileContent, '', '', TRUE);
+  $text = mb_str_replace('<p>&nbsp;</p>', '', $text);
+  $text = mb_str_replace('<p align="left">&nbsp;</p>', '', $text);
+
+  $text = removeExcess($text, '<table border="4" width="210"', '</table>', TRUE);
+//$inFileContent = removeExcess($inFileContent, '', '</div>', TRUE);
+  $text = mb_str_replace('<div align="right">
+          
+        </div>', '', $text);
+  $text = mb_str_replace('<span style="font-size:11.0pt;
+mso-bidi-font-size:10.0pt;font-family:Arial;mso-fareast-font-family:&quot;MS Mincho&quot;">', '<span>', $text);
+  $text = removeExcess($text, '<font size="2">This', '</font>', TRUE);
+  $text = removeExcess($text, '<table border="0" width="100%"
+bgcolor="#FFC8C8"', '</table>', TRUE);
+  $text = removeExcess($text, '<table border="0" width="100%"
+bgcolor="#FFC8C8"', '</table>', TRUE);
+
+  $text = removeExcess($text, '<table style="font-family: Arial, Helvetica; font-size: 8pt" bgcolor="#FFC8C8"', '</table>', TRUE);
+  $text = removeExcess($text, '<p><a href="http://www.chantandbehappy.com/">', '/p>', TRUE);
+  $text = removeExcess($text, '<table border="1" cellpadding="4" cellspacing="1" width="100%">', '</table>', TRUE);
+  $text = removeExcess($text, '<table border="0" width="100%"
+bgcolor="#FFC8C8"', '</table>', TRUE);
+  $text = removeExcess($text, '<p><a
+href="http://www.chantandbehappy.com/', '</p>', TRUE);
+
+  $text = removeExcess($text, '<table border="1" width="100%"
+cellspacing="1" cellpadding="4">
+<tr>', '</table>', TRUE);
+  $text = removeExcess($text, '<table style="font-family: Arial, Helvetica; font-size: 8pt" bordercolordark="#C0C0C0" bgcolor="#FFC8C8" border="2" cellpadding="0" cellspacing="0" width="100%">', '</tbody></table>', TRUE);
+  $text = removeExcess($text, '<td width="100%">&nbsp;</td>
+</tr>
+</tbody>', '</table>', TRUE);
+
+  $text = removeExcess($text, '<table style="font-family: Arial, Helvetica; font-size: 8pt" bordercolordark="#C0C0C0" bgcolor="#FFC8C8" border="2"', '</table>', TRUE);
+  $text = removeExcess($text, '<table border="2" width="100%"', '</table>', TRUE);
+  $text = removeExcess($text, '<p align="center"><a href="http://', '</a></p>', TRUE);
+  $text = removeExcess($text, '<p align="center"><a
+href="http://', '</a></p>', TRUE);
+
+  $text = mb_str_replace('<table bgcolor="#FFC8C8" border="0" cellpadding="0" cellspacing="0" width="100%">
+<tbody><tr>
+<td width="100%">&nbsp;</td>
+</tr>
+</tbody></table>
+</div>
+
+
+
+<p>&nbsp;</p></td>
+<td width="2%"></td>
+<td align="left" valign="top" width="80%">
+<div align="right">
+<table align="right" border="0" cellpadding="0" cellspacing="0" width="30%">
+<tbody><tr>
+<td width="100%">
+
+
+
+</td>
+</tr>
+</tbody></table>', '', $text);
+//$inFileContent = mb_str_replace('', '', $inFileContent);
+  $text = removeExcess($text, '<style ', '</style>', TRUE);
+  $text = removeExcess($text, '<table border="1" width="100%" bgcolor="#FFC8C8">', '</table>', TRUE);
+  $text = mb_str_replace('<td width="100%">&nbsp;</td>', '', $text);
+
+  $text = removeExcess($text, '<td valign="top" width="15%" bgcolor="#FFC8C8" style="border-right: 1 solid #C0C0C0;', '</td>', TRUE);
+  $text = removeExcess($text, '<td valign="top" width="15%"
+bgcolor="#FFC8C8"', '</td>', TRUE);
+  $text = removeExcess($text, '<td width="100%" bgcolor="#FFFFCE">', '</font></td>', TRUE);
+  $text = removeExcess($text, '<div align="right">', '</div>', TRUE);
+  $text = removeExcess($text, '<div align="right">', '</div>', TRUE);
+  $text = removeExcess($text, '<p align="left"><font color="#cc0000" face="Times New Roman, Georgia, Times" size="4"><b>Go to the &#147;<a href="http://', '</font></p>', TRUE);
+  $text = truncateText($text, '', '<p align="center"><strong><font');
+  $text = mb_str_replace('<p align="center"><a name="top"></a><small>Hare Krishna!', '', $text);
+  return $text;
+}
